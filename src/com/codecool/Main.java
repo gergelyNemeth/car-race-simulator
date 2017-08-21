8caa480e7f01133f3f8fcae108a81911bc6c6478
Gergely Németh <ngerle@gmail.com>
2017-08-22 01:06:18 +0200

    Implement the parts of simulateRace() and printRaceResults() related to cars

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -26,8 +26,34 @@ public class Main {
         }
     }
 
+    private static void simulateRace() {
+        // Move the vehicles for 50 hours
+        for (int i = 0; i < 50; i++) {
+            for (Car car : cars) {
+                car.moveForAnHour();
+            }
+        }
+        if (isRaining()) {
+            System.out.println("It's a rainy day!");
+        }
+    }
+
+    private static void printRaceResults() {
+        cars.sort((car1, car2) -> car2.distance - car1.distance);
+        int counter = 1;
+        for (Car car : cars) {
+            System.out.print(counter + ": ");
+            System.out.print(car.name + " - ");
+            System.out.print(car.distance + " km - ");
+            System.out.println(car.getClass().getSimpleName());
+            counter++;
+        }
+    }
+
 
     public static void main(String[] args) {
         createVehicles();
+        simulateRace();
+        printRaceResults();
     }
 }
