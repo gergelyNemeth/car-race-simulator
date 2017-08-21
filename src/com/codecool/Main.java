a28a1868efbd53e99e657d1ce536a03dd265c5c4
Gergely Németh <ngerle@gmail.com>
2017-08-22 00:56:10 +0200

    Implement isRaining() in Main class

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -1,8 +1,17 @@
 package com.codecool;
 
+import java.util.*;
+
 public class Main {
 
+    private static Boolean isRaining() {
+        Random randomGenerator = new Random();
+        int random = randomGenerator.nextInt(10);
+        // 30% chance to rain
+        return random < 3;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(isRaining());
     }
 }
