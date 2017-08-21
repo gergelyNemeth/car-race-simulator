b44275156b15b592430e3f92fbe8afa6235873d6
Gergely Németh <ngerle@gmail.com>
2017-08-22 00:47:43 +0200

    Implement Car class

--- src/com/codecool/Car.java
+++ src/com/codecool/Car.java
@@ -1,4 +1,47 @@
 package com.codecool;
 
-public class Car {
+import java.util.Random;
+
+class Car {
+    String name;
+    private int normalSpeed;
+    private int distanceTraveled;
+    int distance;
+    private static int speedLimit;
+
+    private static final int MINSPEED = 80;
+    private static final int MAXSPEED = 120;
+
+    static void setSpeedLimit(int limit) {
+        speedLimit = limit;
+    }
+
+    Car() {
+        Random randomGenerator = new Random();
+        // Speed
+        Float random = randomGenerator.nextFloat();
+        normalSpeed = MINSPEED + (int) (random * (MAXSPEED - MINSPEED));
+        if (speedLimit > 0) {
+            normalSpeed = speedLimit;
+        }
+        // Name
+        String[] names = {"Elysium", "Specter", "Wolf", "Legacy", "Buffalo", "Centaur",
+                "Prestige", "Intro", "Blend", "Evolution", "Crest", "Thunder",
+                "Nebula", "Chase", "Shadow", "Quicksilver", "Trailblazer", "Guardian",
+                "Blast", "Enigma"};
+        int i = 0;
+        int j = 0;
+        while (i == j) {
+            i = randomGenerator.nextInt(20);
+            j = randomGenerator.nextInt(20);
+        }
+        name = names[i] + " " + names[j];
+
+        distanceTraveled = 0;
+    }
+
+    void moveForAnHour() {
+        distanceTraveled += 1;
+        distance = normalSpeed * distanceTraveled;
+    }
 }
