98594fefe62562aba8f9bd969d2d40a87e92857d
Gergely Németh <ngerle@gmail.com>
2017-08-22 01:04:52 +0200

    Implement the parts of createVehicles() related to cars in Main class

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 public class Main {
 
+    private static List<Car> cars = new ArrayList<>();
+
     private static Boolean isRaining() {
         Random randomGenerator = new Random();
         int random = randomGenerator.nextInt(10);
@@ -11,7 +13,21 @@ public class Main {
         return random < 3;
     }
 
+    private static void createVehicles() {
+        Random randomGenerator = new Random();
+        int random = randomGenerator.nextInt(10);
+        // 30% chance to limit the cars speed to 70 km/h
+        if (random <= 3) {
+            Car.setSpeedLimit(70);
+        }
+        // Create 10 cars
+        for (int i = 0; i < 10; i++) {
+            cars.add(new Car());
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(isRaining());
+        createVehicles();
     }
 }
