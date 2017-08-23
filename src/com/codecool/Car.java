1d040fd294ad298b614acf94034fac381dc2a0d8
Gergely Németh <ngerle@gmail.com>
2017-08-23 22:59:44 +0200

    Put code of speed calculation and name creation into methods in Car class

--- src/com/codecool/Car.java
+++ src/com/codecool/Car.java
@@ -1,5 +1,7 @@
 package com.codecool;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 class Car {
@@ -8,30 +10,48 @@ class Car {
     private int normalSpeed;
     private int actualSpeed;
     private int distanceTraveled;
+    private static List<String> usedNames = new ArrayList<>();
 
     Car() {
+        actualSpeed = calculateNormalSpeed();
+        name = createNewName();
+        distanceTraveled = 0;
+    }
+
+    private int calculateNormalSpeed() {
         Random randomGenerator = new Random();
-        // Speed
         int minSpeed = 80;
         int maxSpeed = 110;
         double random = randomGenerator.nextDouble();
         normalSpeed = minSpeed + (int) (random * (maxSpeed - minSpeed));
-        actualSpeed = normalSpeed;
-        // Name
+
+        return normalSpeed;
+    }
+
+    private String createNewName() {
         String[] names = {"Elysium", "Specter", "Wolf", "Legacy", "Buffalo", "Centaur",
                 "Prestige", "Intro", "Blend", "Evolution", "Crest", "Thunder",
                 "Nebula", "Chase", "Shadow", "Quicksilver", "Trailblazer", "Guardian",
                 "Blast", "Enigma", "Centurion", "Bolt", "Vortex", "Fragment", "Freedom",
                 "Renegade", "Hydra", "Stardust", "Liberty", "Desire"};
+        Random randomGenerator = new Random();
         int i = 0;
         int j = 0;
-        while (i == j) {
-            i = randomGenerator.nextInt(30);
-            j = randomGenerator.nextInt(30);
+        Boolean nameNotUsed = true;
+        String newName = "";
+        while (nameNotUsed) {
+            while (i == j) {
+                i = randomGenerator.nextInt(30);
+                j = randomGenerator.nextInt(30);
+            }
+            newName = names[i] + " " + names[j];
+            if (!usedNames.contains(newName)) {
+                usedNames.add(newName);
+                nameNotUsed = false;
+            }
+            i = j;
         }
-        name = names[i] + " " + names[j];
-
-        distanceTraveled = 0;
+        return newName;
     }
 
     static void setSpeedLimit(int limit) {
